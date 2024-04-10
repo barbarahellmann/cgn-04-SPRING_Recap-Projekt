3097521670b412052fef23505b1f56292eae6f78
barbarahellmann <babshellmann@web.de>
2024-04-10 17:25:06 +0200

    Integrated more methods but it´s not updating Super Kanban

--- src/main/java/org/example/cgn04spring_recapprojekt/model/Todo.java
+++ src/main/java/org/example/cgn04spring_recapprojekt/model/Todo.java
@@ -2,8 +2,9 @@ package org.example.cgn04spring_recapprojekt.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("Todo")
-public record Todo(String id, String description, Status status) {
+public record Todo(@Id String id, String description, Status status) {
 }
