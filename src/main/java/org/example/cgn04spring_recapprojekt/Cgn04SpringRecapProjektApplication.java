370f1c2aeafd08aee833c2405195476194a278d7
barbarahellmann <babshellmann@web.de>
2024-04-10 13:10:47 +0200

    Initial commit

---
+++ src/main/java/org/example/cgn04spring_recapprojekt/Cgn04SpringRecapProjektApplication.java
@@ -0,0 +1,13 @@
+package org.example.cgn04spring_recapprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cgn04SpringRecapProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cgn04SpringRecapProjektApplication.class, args);
+	}
+
+}
